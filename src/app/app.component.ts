0a2fc66c5636bed057da53a61b0e4f7b70929239
Igor645 <igormartic6@gmail.com>
2025-02-03 01:35:16 +0100

    scroll to top on page switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, PLATFORM_ID, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { Router, NavigationEnd } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +12,20 @@ import { MainLayoutComponent } from './components/main-layout/main-layout.compon
   imports: [CommonModule, RouterModule, MainLayoutComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit{
+  constructor(private router: Router, @Inject(PLATFORM_ID) private platformId: any) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (isPlatformBrowser(this.platformId)) {
+            window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+          });
+        }
+      }
+    });
+  }
+  
+}
