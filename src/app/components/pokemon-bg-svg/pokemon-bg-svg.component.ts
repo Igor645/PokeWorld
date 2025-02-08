706f8615dba44a6a0438b9ffe1325aefa5d2ff13
Igor645 <igormartic6@gmail.com>
2025-02-08 23:06:27 +0100

    feat: add Pokemon background SVG component and integrate it into various components

---
+++ src/app/components/pokemon-bg-svg/pokemon-bg-svg.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  selector: 'app-pokemon-bg-svg',
+  imports: [CommonModule],
+  templateUrl: './pokemon-bg-svg.component.html',
+  styleUrls: ['./pokemon-bg-svg.component.css']
+})
+export class PokemonBgSvgComponent {
+  @Input() color: string = 'var(--svg-color)';
+}
