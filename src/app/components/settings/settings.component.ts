4bbb7b1710de1284815a5ae7f764fc0cf8225f7b
Igor645 <igormartic6@gmail.com>
2025-02-08 23:22:59 +0100

    feat: replace dark mode button with slide toggle and update styles

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { LanguageSelectorComponent } from '../language-selector/language-selector.component';
 import { SettingsService } from '../../services/settings.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'app-settings',
-  imports: [CommonModule, MatIconModule, MatCardModule, MatButtonModule, LanguageSelectorComponent],
+  imports: [CommonModule, MatIconModule, FormsModule, MatSlideToggleModule, MatCardModule, MatButtonModule, LanguageSelectorComponent],
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
@@ -20,11 +22,17 @@ export class SettingsComponent implements OnInit {
   constructor(private settingsService: SettingsService) {}
 
   ngOnInit() {
-    const savedDarkMode = this.settingsService.getSetting<boolean>('darkMode');
-    this.isDarkMode = savedDarkMode ?? false;
+    this.isDarkMode = this.settingsService.getSetting<boolean>('darkMode') ?? false;
+  }
+
+  toggleDarkMode() {
+    console.log('toggleDarkMode');
+    this.settingsService.setSetting('darkMode', this.isDarkMode);
 
     if (this.isDarkMode) {
       document.documentElement.classList.add('dark-theme');
+    } else {
+      document.documentElement.classList.remove('dark-theme');
     }
   }
 
@@ -37,17 +45,6 @@ export class SettingsComponent implements OnInit {
     const target = event.target as HTMLElement;
     if (!target.closest('.settings-container') && !target.closest('.settings-cog')) {
       this.showSettings = false;
-    }
-  }
-
-  toggleDarkMode() {
-    this.isDarkMode = !this.isDarkMode;
-    this.settingsService.setSetting('darkMode', this.isDarkMode);
-
-    if (this.isDarkMode) {
-      document.documentElement.classList.add('dark-theme');
-    } else {
-      document.documentElement.classList.remove('dark-theme');
     }
   }
 }
