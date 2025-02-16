7a5fa2b84a78489cf5638a126b9b240f1e0f2b48
Igor645 <igormartic6@gmail.com>
2025-02-16 21:07:15 +0100

    fix: streamline static file serving in server.js for improved efficiency

--- server.js
+++ server.js
@@ -2,14 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const staticPath = path.join(__dirname, 'dist', 'poke-world-angular', 'browser');
+app.use(express.static(path.join(__dirname, 'dist', 'poke-world-angular', 'browser')));
 
-// Serve static files from the browser folder
-app.use(express.static(staticPath));
-
-// For all GET requests, send back index.html so that Angular can handle routing
 app.get('*', (req, res) => {
-  res.sendFile(path.join(staticPath, 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'poke-world-angular', 'browser', 'index.html'));
 });
 
 const port = process.env.PORT || 8080;
