abf42e5eac6c73804f0a5cd8c5734a6581ac071d
Igor645 <igormartic6@gmail.com>
2025-02-16 20:44:31 +0100

    fix: increase maximum warning size for initial budget and update entry point mappings in manifest

--- dist/poke-world-angular/server/angular-app-manifest.mjs
+++ dist/poke-world-angular/server/angular-app-manifest.mjs
@@ -5,10 +5,17 @@ export default {
   baseHref: '/',
   locale: undefined,
   routes: undefined,
-  entryPointToBrowserMapping: {},
+  entryPointToBrowserMapping: {
+  "node_modules/@angular/animations/fesm2022/browser.mjs": [
+    {
+      "path": "chunk-I4FDTYXY.js",
+      "dynamicImport": false
+    }
+  ]
+},
   assets: {
-    'index.csr.html': {size: 9822, hash: 'e50d0414d3bec4d7ddb95f0ad2ae6eba06605ed58dc622cd610a119665dcb542', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 8906, hash: '99b973374eb7a0ab9a5ab3645c01267efd9ebb9b5bbb2ae1222d722f182fe99f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-EAJ3YX6B.css': {size: 1328, hash: 'T1q+0EzjbfQ', text: () => import('./assets-chunks/styles-EAJ3YX6B_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 32657, hash: '0c06e2b124ca458242e46efc8dd499119c339f04aee43b1a8ebdcc102f6c9f51', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 24797, hash: 'bda79213c4fc66ed23d115abc13508d360889184d8c832bb668f64390265baeb', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-ZKVL2H2A.css': {size: 9645, hash: 'tQc+4X09LRU', text: () => import('./assets-chunks/styles-ZKVL2H2A_css.mjs').then(m => m.default)}
   },
 };
