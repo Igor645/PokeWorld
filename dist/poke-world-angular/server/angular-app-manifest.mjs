ac3d4d281d769cdd12ae3058580fc5e3005736f6
Igor645 <igormartic6@gmail.com>
2025-02-16 21:18:14 +0100

    fix: update static file references and improve server configuration for Angular app

--- dist/poke-world-angular/server/angular-app-manifest.mjs
+++ dist/poke-world-angular/server/angular-app-manifest.mjs
@@ -8,14 +8,14 @@ export default {
   entryPointToBrowserMapping: {
   "node_modules/@angular/animations/fesm2022/browser.mjs": [
     {
-      "path": "chunk-I4FDTYXY.js",
+      "path": "chunk-WOWNJSKN.js",
       "dynamicImport": false
     }
   ]
 },
   assets: {
-    'index.csr.html': {size: 32657, hash: '0c06e2b124ca458242e46efc8dd499119c339f04aee43b1a8ebdcc102f6c9f51', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 24797, hash: 'bda79213c4fc66ed23d115abc13508d360889184d8c832bb668f64390265baeb', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-ZKVL2H2A.css': {size: 9645, hash: 'tQc+4X09LRU', text: () => import('./assets-chunks/styles-ZKVL2H2A_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 32657, hash: 'fc5eb0083a641913094d8e52fde28722784fefe4126c110f739a225ff610150e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 24797, hash: '8527b942861bff6298448153d8383a2d6a2410ca5c9be7d980f46272590230d1', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-OW7XI5CF.css': {size: 9597, hash: 'QASYudcs/9A', text: () => import('./assets-chunks/styles-OW7XI5CF_css.mjs').then(m => m.default)}
   },
 };
--- server.js
+++ server.js
@@ -2,13 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Define the static folder for client assets
 const staticPath = path.join(__dirname, 'dist', 'poke-world-angular', 'browser');
 
-// Serve static files first
 app.use(express.static(staticPath));
 
-// Fallback to index.html for any other routes (for Angular's routing)
 app.get('*', (req, res) => {
   res.sendFile(path.join(staticPath, 'index.html'));
 });
