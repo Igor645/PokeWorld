ff11d9a772f164d34d6a1add68a8b8b70ec42982
Igor645 <igormartic6@gmail.com>
2025-05-24 01:28:45 +0200

    feat: add NotFoundComponent with routing and styling for 404 error page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { DexOverviewComponent } from './components/overview/dex-overview/dex-overview.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { PokemonDetailsComponent } from './components/details/pokemon/pokemon-details/pokemon-details.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', component: DexOverviewComponent },
   { path: 'pokemon/:speciesIdOrName', component: PokemonDetailsComponent },
-  { path: '**', redirectTo: '/', pathMatch: 'full' }
+  { path: '**', component: NotFoundComponent }
 ];
