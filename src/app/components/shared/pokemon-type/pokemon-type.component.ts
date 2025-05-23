e0af439f35d6882dc06000620e637a59480b30de
Igor645 <igormartic6@gmail.com>
2025-05-24 01:18:12 +0200

    refactor: improve component structure and enhance dark mode support

--- src/app/components/shared/pokemon-type/pokemon-type.component.ts
+++ src/app/components/shared/pokemon-type/pokemon-type.component.ts
@@ -1,16 +1,25 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnDestroy,
+  OnInit,
+  Optional,
+  Self
+} from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { PokemonUtilsService } from '../../../utils/pokemon-utils';
 import { SettingsService } from '../../../services/settings.service';
 import { Subscription } from 'rxjs';
+import { InteractiveHostDirective } from '../directives/interactive-host.directive';
 
 @Component({
   selector: 'app-pokemon-type',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './pokemon-type.component.html',
-  styleUrls: ['./pokemon-type.component.css']
+  styleUrls: ['./pokemon-type.component.css'],
+  hostDirectives: [InteractiveHostDirective]
 })
 export class PokemonTypeComponent implements OnInit, OnDestroy {
   @Input() pokemonType: any;
@@ -20,11 +29,6 @@ export class PokemonTypeComponent implements OnInit, OnDestroy {
   localizedTypeName: string = 'Unknown';
   private darkModeSubscription!: Subscription;
   private languageSubscription!: Subscription;
-
-  constructor(
-    public pokemonUtils: PokemonUtilsService,
-    private settingsService: SettingsService
-  ) { }
 
   typeColorsLight: { [key: string]: string } = {
     normal: '#B9B9AA',
@@ -37,43 +41,68 @@ export class PokemonTypeComponent implements OnInit, OnDestroy {
   };
 
   typeColorsDark: { [key: string]: string } = {
-    normal: '#8B8B77',
-    fire: '#B22222', water: '#1E4D9B', electric: '#C9A917',
-    grass: '#507D2A', ice: '#5E9EA0', fighting: '#7D1F1A',
-    poison: '#682A68', ground: '#9C773C', flying: '#7A6DAF',
-    psychic: '#A13959', bug: '#6D7815', rock: '#786824',
-    ghost: '#493963', dragon: '#4C1D95', dark: '#3D2D22',
-    steel: '#787887', fairy: '#9B6470'
+    normal: '#A0A08C',
+    fire: '#D75A4A',
+    water: '#3A6DB2',
+    electric: '#D4B63A',
+    grass: '#5E9C5E',
+    ice: '#7FC8CC',
+    fighting: '#A3453E',
+    poison: '#8F5A9C',
+    ground: '#B9985A',
+    flying: '#8F8ED6',
+    psychic: '#D16C88',
+    bug: '#8E9E3E',
+    rock: '#A08A50',
+    ghost: '#7C6AB0',
+    dragon: '#6C4BCC',
+    dark: '#5C4C44',
+    steel: '#9BA9C0',
+    fairy: '#D18EA6'
   };
 
+  constructor(
+    public pokemonUtils: PokemonUtilsService,
+    private settingsService: SettingsService,
+    @Self() @Optional() private interactiveHost?: InteractiveHostDirective
+  ) { }
+
   ngOnInit() {
-    this.darkModeSubscription = this.settingsService.watchSetting<boolean>('darkMode')
+    this.darkModeSubscription = this.settingsService
+      .watchSetting<boolean>('darkMode')
       .subscribe(isDark => {
         this.isDarkMode = isDark ?? false;
       });
 
-    this.languageSubscription = this.pokemonUtils.watchLanguageChanges()
+    this.languageSubscription = this.pokemonUtils
+      .watchLanguageChanges()
       .subscribe(() => {
         this.updateLocalizedTypeName();
+        this.setNavigationLink();
       });
 
     this.updateLocalizedTypeName();
+    this.setNavigationLink();
   }
 
   ngOnDestroy() {
-    if (this.darkModeSubscription) {
-      this.darkModeSubscription.unsubscribe();
-    }
-    if (this.languageSubscription) {
-      this.languageSubscription.unsubscribe();
-    }
+    this.darkModeSubscription?.unsubscribe();
+    this.languageSubscription?.unsubscribe();
   }
 
   updateLocalizedTypeName() {
-    if (this.pokemonType && this.pokemonType.pokemon_v2_typenames) {
-      this.localizedTypeName = this.pokemonUtils.getNameByLanguage(this.pokemonType.pokemon_v2_typenames);
+    if (this.pokemonType?.pokemon_v2_typenames) {
+      this.localizedTypeName = this.pokemonUtils.getNameByLanguage(
+        this.pokemonType.pokemon_v2_typenames
+      );
     } else {
       this.localizedTypeName = 'Unknown';
+    }
+  }
+
+  setNavigationLink() {
+    if (this.interactiveHost && this.localizedTypeName !== 'Unknown') {
+      this.interactiveHost.href = ['/type', this.localizedTypeName.toLowerCase()];
     }
   }
 
