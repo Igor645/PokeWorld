f060a940edcefd9efc467590415d31621aa9f938
Igor645 <igormartic6@gmail.com>
2025-05-24 02:12:11 +0200

    feat: add app loader with background styling during application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,10 @@ import { appConfig } from './app/app.config';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 bootstrapApplication(AppComponent, appConfig)
+  .then(() => {
+    const loader = document.getElementById('app-loader');
+    if (loader) {
+      loader.remove();
+    }
+  })
   .catch(err => console.error(err));
